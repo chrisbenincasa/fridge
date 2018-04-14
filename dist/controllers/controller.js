5f96325fcc8d207f41f8164d059623fb1ba79cdb
Christian Benincasa <chrisbenincasa@gmail.com>
2018-04-14 13:26:28 -0400

    Forget dist/

--- dist/controllers/controller.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Controller {
-    constructor(router) {
-        this.router = router;
-    }
-}
-exports.Controller = Controller;
-//# sourceMappingURL=controller.js.map
